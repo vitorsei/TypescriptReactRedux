0d2db96dc4850ea00be58545bd35f44abff58d17
vitoriwamura <seiji.vitor@gmail.com>
2017-02-22 11:15:43 +1100

    Putting Immutable js back

--- src/reducers/counter.ts
+++ src/reducers/counter.ts
@@ -1,33 +1,28 @@
-import {
-    INCREMENT_COUNTER,
-    DECREMENT_COUNTER,
-    LOGOUT_USER
-} from '../constants';
+import {  INCREMENT_COUNTER,  DECREMENT_COUNTER } from '../constants';
 import {fromJS} from 'immutable';
 import {IncrementCounterAction, DecrementCounterAction, OtherAction} from '../actions/counter';
 
-
 const INITIAL_STATE = fromJS({
-    count: 0,
+  count: 0,
 });
 
 type CounterAction =
-    IncrementCounterAction |
-        DecrementCounterAction |
-        OtherAction
+  IncrementCounterAction |
+    DecrementCounterAction |
+    OtherAction
 
 function counterReducer(state = INITIAL_STATE, action: CounterAction = OtherAction) {
 
-    switch (action.type) {
-        case INCREMENT_COUNTER:
-            return state.update('count', (value) => value + action.by);
+  switch (action.type) {
+    case INCREMENT_COUNTER:
+      return state.update('count', (value) => value + action.by);
 
-        case DECREMENT_COUNTER:
-            return state.update('count', (value) => value - 1);
+    case DECREMENT_COUNTER:
+      return state.update('count', (value) => value - 1);
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }
 
 
